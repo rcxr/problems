3a20a394efb4fe5af2eaeb8df56d630cabaa82e1
Rafa Diaz <racrux@gmail.com>
2017-11-04 15:35:02 -0700

    omegaUp 0-Multiplicacion-basica

---
+++ omegaUp/0-Multiplicacion-basica.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// https://omegaup.com/arena/problem/0-Multiplicacion-basica
+int main() {
+  int num1, num2;
+
+  scanf("%d", &num1);
+  scanf("%d", &num2);
+
+  printf("%d", (num1 * num2));
+}
