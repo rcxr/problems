1a09ab7bb11cc917102fd1f71de6343ccff9a4c4
Rafa Díaz <racrux@gmail.com>
2018-02-08 13:34:22 -0800

    codeforces c922C nit updates

--- codeforces/rcxr/c922C/src/main.cpp
+++ codeforces/rcxr/c922C/src/main.cpp
@@ -3,42 +3,15 @@
 #include <iostream>
 #include <unordered_map>
 
-void process(std::unordered_map<unsigned long, unsigned long>& primes, unsigned long n) {
-  for (auto prime : primes) {
-    for (unsigned long i = 0; i < prime.second; ++i) {
-      if (n % prime.first) {
-        break;
-      }
-      n /= prime.first;
-    }
-  }
-
-  if (1 < n) {
-    if (primes.end() == primes.find(n)) {
-      primes[n] = 0;
-    }
-    ++primes[n];
-  }
-}
-
 int main() {
-  unsigned long n, k;
+  long n, k;
   std::cin >> n >> k;
+  ++n;
 
-  std::unordered_map<unsigned long, unsigned long> primes;
-  for (unsigned long i = 2; i <= k; ++i) {
-    process(primes, i);
-  }
-
-  n++;
-
-  for (auto prime : primes) {
-    for (unsigned long i = 0; i < prime.second; ++i) {
-      if (n % prime.first) {
-        std::cout << "No";
-        return 0;
-      }
-      n /= prime.first;
+  for (long i = 2; i <= k; ++i) {
+    if (n % i) {
+      std::cout << "No";
+      return 0;
     }
   }
 
