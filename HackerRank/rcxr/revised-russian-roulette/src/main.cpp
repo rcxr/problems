f1601d1eb68756978b6635f9be49f6c838c4c8df
Rafa Díaz <racrux@gmail.com>
2018-02-08 19:54:05 -0800

    HackerRank revised-russian-roulette done!

--- HackerRank/rcxr/revised-russian-roulette/src/main.cpp
+++ HackerRank/rcxr/revised-russian-roulette/src/main.cpp
@@ -1,11 +1,29 @@
-// <link to the problem here>
+// https://www.hackerrank.com/contests/w36/challenges/revised-russian-roulette
 
 #include "bits/stdc++.h"
 
 int main() {
-  long n;
+  unsigned n;
   std::cin >> n;
 
-  std::cout << n;
+  auto min = 0u;
+  auto max = 0u;
+  bool previous = false;
+  while (n--) {
+    bool door;
+    std::cin >> door;
+    if (door) {
+      ++max;
+      if (previous) {
+        previous = false;
+      } else {
+        ++min;
+        previous = true;
+      }
+    } else {
+      previous = false;
+    }
+  }
+  std::cout << min << " " << max;
   return 0;
 }
