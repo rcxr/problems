77d6935e36b59e96d7e5006c51d85cac5151e96e
Rafa Díaz <racrux@gmail.com>
2018-02-10 17:08:28 -0800

    HackerRank a-race-against-time init

---
+++ HackerRank/rcxr/a-race-against-time/src/main.cpp
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/contests/w36/challenges/a-race-against-time
+
+#include "bits/stdc++.h"
+
+int main() {
+  long n;
+  std::cin >> n;
+
+  std::cout << n;
+  return 0;
+}
