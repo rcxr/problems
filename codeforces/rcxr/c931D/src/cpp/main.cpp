508717f7e1d80dbf579c8e08c24dfe151b6921f8
Rafa Díaz <racrux@gmail.com>
2018-03-09 16:43:14 -0800

    codeforces c931D

--- codeforces/rcxr/c931D/src/cpp/main.cpp
+++ codeforces/rcxr/c931D/src/cpp/main.cpp
@@ -1,40 +1,48 @@
 // http://codeforces.com/contest/931/problem/D
 
 #include "bits/stdc++.h"
-#include <unordered_set>
-
-void copyLogs(std::unordered_set<int>& nextLog, std::unordered_set<int>& log) {
-  for (auto& t : log) {
-    auto temp = nextLog.find(t + 1);
-    if (nextLog.end() == temp) {
-      nextLog.insert(t + 1);
-    } else {
-      nextLog.erase(temp);
-    }
-  }
-}
+#include <unordered_map>
 
 int main() {
+  // Reading number of branches
   int n;
   std::cin >> n;
 
-  std::vector<int> nextBranch(n);
+  // This stores the distance to the root
+  std::vector<int> depths(n);
+  // This counts the instances of the different depths
+  std::unordered_map<int, int> depthCounts;
+
+  // It's known that the depth of the first branch is zero
+  depths[0] = 0;
+  // Set the counter for the zero depth to 1
+  depthCounts[0] = 1;
+
+  // Read the tree structure
   for (int i = 1; i < n; ++i) {
     int temp;
     std::cin >> temp;
-    nextBranch[i] = temp - 1;
+    // The current branch will have its apples move to this other branch (which we are calling next branch)
+    int nextBranch = temp - 1;
+    // The depth of this branch will be 1 more than the next branch
+    depths[i] = depths[nextBranch + 1;
+    // Increase the counter for this depth
+    if (depthCounts.end() == depthCounts.find(depths[i])) {
+      depthCounts[depths[i]] = 1;
+    } else {
+      ++depthCounts[depths[i]];
+    }
   }
 
-  std::vector<std::unordered_set<int>> logs(n);
-  for (auto& log : logs) {
-    log.insert(0);
+  int result = 0;
+  // Count how many depths have an odd number of occurrences
+  for (auto& depthCount : depthCounts) {
+    if (depthCount.second % 2) {
+      ++result;
+    }
   }
 
-  for (int i = n - 1; 0 < i; --i) {
-    copyLogs(logs[nextBranch[i]], logs[i]);
-  }
-
-  std::cout << logs[0].size();
+  std::cout << result;
 
   return 0;
 }
