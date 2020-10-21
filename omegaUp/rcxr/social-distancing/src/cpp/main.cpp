6763c3ed938391b0ccf09b1fc8f1d001215055da
Rafa Díaz <racrux@gmail.com>
2020-10-21 16:02:43 -0700

    safe-distancing: classes

--- omegaUp/rcxr/social-distancing/src/cpp/main.cpp
+++ omegaUp/rcxr/social-distancing/src/cpp/main.cpp
@@ -2,41 +2,74 @@
 
 #include "bits/stdc++.h"
 
-class Customer {
+#define X 0
+#define Y 1
+#define SAFE_DISTANCE 16
+
+class Marchante {
 public:
-  Customer(int groupId, int x, int y) {
-    this->groupId = groupId;
-    this->x = x;
-    this->y = y;
+  Marchante(int id, int x, int y) : id(id), groupId(id), p({x, y}) {
   }
+
+  bool isSafe(const Marchante *c) const {
+    int dx = c->p[X] - p[X];
+    int dy = c->p[Y] - p[Y];
+    return dx * dx + dy * dy > SAFE_DISTANCE;
+  }
+
 private:
+  const int id;
+  const std::array<int, 2> p;
   int groupId;
-  int x;
-  int y;
-  std::set<int> neighborsH;
-  std::set<int> neighborsV;
+  std::set<int> neighbors0;
+  std::set<int> neighbors1;
 };
 
-bool isPasilloSafe(std::vector<Customer> &customers, int w, int l) {
-  return true;
-}
+class Mercado {
+public:
+  Mercado(int w, int l) : w(w), l(l) {}
+
+  ~Mercado() {
+    for(auto m : marchantes) {
+      delete m;
+    }
+    marchantes.clear();
+  }
+
+  void addMarchante(int id, int x, int y) {
+    marchantes.push_back(new Marchante(id, x, y));
+  }
+
+  bool isSafe() {
+    populateNeighbors(X);
+    populateNeighbors(Y);
+    return true;
+  }
+private:
+  std::vector<Marchante *> marchantes;
+  const int w;
+  const int l;
+
+  void populateNeighbors(int dimension) {}
+};
 
 int main() {
   int n;
   std::cin >> n;
 
   for (int i = 0; i < n; ++i) {
-    int c, w, l;
-    std::cin >> c >> w >> l;
+    int m, w, l;
+    std::cin >> m >> w >> l;
 
-    std::vector<Customer> customers;
-    for (int j = 0; j < c; ++j) {
+    Mercado mercado(w, l);
+
+    for (int j = 0; j < m; ++j) {
       int x, y;
       std::cin >> x >> y;
-      customers.push_back(Customer(j, x, y));
+      mercado.addMarchante(j, x, y);
     }
 
-    std::cout << (isPasilloSafe(customers, w, l) ? "SAFE" : "RISKY") << std::endl;
+    std::cout << (mercado.isSafe() ? "SAFE" : "RISKY") << std::endl;
   }
 
   return 0;
