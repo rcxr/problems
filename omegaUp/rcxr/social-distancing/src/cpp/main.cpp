8cd58001a7dd472d852c4fd70a1583c5c175726b
Rafa Díaz <racrux@gmail.com>
2020-10-21 11:12:57 -0700

    social-distancing: baseline 2

--- omegaUp/rcxr/social-distancing/src/cpp/main.cpp
+++ omegaUp/rcxr/social-distancing/src/cpp/main.cpp
@@ -2,10 +2,42 @@
 
 #include "bits/stdc++.h"
 
+class Customer {
+public:
+  Customer(int groupId, int x, int y) {
+    this->groupId = groupId;
+    this->x = x;
+    this->y = y;
+  }
+private:
+  int groupId;
+  int x;
+  int y;
+  std::set<int> neighborsH;
+  std::set<int> neighborsV;
+};
+
+bool isPasilloSafe(std::vector<Customer> &customers, int w, int l) {
+  return true;
+}
+
 int main() {
   int n;
   std::cin >> n;
 
-  std::cout << n;
+  for (int i = 0; i < n; ++i) {
+    int c, w, l;
+    std::cin >> c >> w >> l;
+
+    std::vector<Customer> customers;
+    for (int j = 0; j < c; ++j) {
+      int x, y;
+      std::cin >> x >> y;
+      customers.push_back(Customer(j, x, y));
+    }
+
+    std::cout << (isPasilloSafe(customers, w, l) ? "SAFE" : "RISKY") << std::endl;
+  }
+
   return 0;
 }
